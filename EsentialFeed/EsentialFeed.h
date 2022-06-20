cf5ef145c221ba93dadf465b3308f87f934b854c
Amir Daliri <amir.daliri.github@gmail.com>
2022-06-20 21:03:51 +0300

    Initial Commit

---
+++ EsentialFeed/EsentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  EsentialFeed.h
+//  EsentialFeed
+//
+//  Created by Amir Daliri on 20.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EsentialFeed.
+FOUNDATION_EXPORT double EsentialFeedVersionNumber;
+
+//! Project version string for EsentialFeed.
+FOUNDATION_EXPORT const unsigned char EsentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EsentialFeed/PublicHeader.h>
+
+
