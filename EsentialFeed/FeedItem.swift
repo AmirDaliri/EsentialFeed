770c7f3fa5f695a362a548c8d35479d8a8b02a1c
Amir Daliri <amir.daliri.github@gmail.com>
2022-06-20 21:07:20 +0300

    Create the FeedItem model

---
+++ EsentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EsentialFeed
+//
+//  Created by Amir Daliri on 20.06.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let iamgeURL: URL
+}
